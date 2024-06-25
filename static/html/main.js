fa1a75e39d70da414115c3f645d77d8a33d6384a
Hermesgb <hermesgabriel@gmail.com>
2024-06-25 10:58:03 +0200

    Las cartas giran al hacer clic

---
+++ static/html/main.js
@@ -0,0 +1,6 @@
+function flipCard() {
+    this.classList.toggle('flip');
+  }
+  
+  const cards = document.querySelectorAll('.memory-card');
+  cards.forEach(card => card.addEventListener('click', flipCard));
