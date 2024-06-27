4c101ab4d4c4161e97de3f24a710f37dd9e13437
Hermesgb <hermesgabriel@gmail.com>
2024-06-27 10:16:00 +0200

    Las cartas se dan la vuelta si no son iguales y se mantienen si lo son.

--- static/html/main.js
+++ static/html/main.js
@@ -1,6 +1,50 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
-  }
-  
-  const cards = document.querySelectorAll('.memory-card');
-  cards.forEach(card => card.addEventListener('click', flipCard));
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
