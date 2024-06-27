f355c8a44e251d17e941030f3772faed0bfd1f41
Muscardina <vergonzalezsainz@gmail.com>
2024-06-27 10:15:42 +0200

    Introducida una función que identifica los pares de cartas y les vuelve a dar la vuelta si no son iguales

--- static/html/main.js
+++ static/html/main.js
@@ -1,12 +1,43 @@
-function flipCard() {
-    this.classList.toggle('flip');
-  }
-  
-  const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.memory-card');
+
+  let hasFlippedCard = false;
+  let firstCard, secondCard;
+
+  function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+     return;
+   }
+
+   secondCard = this;
+   hasFlippedCard = false;
+
+   checkForMatch();
+ }
+
+ function checkForMatch() {
+   if (firstCard.dataset.framework === secondCard.dataset.framework) {
+     disableCards();
+     return;
+   }
+
+   unflipCards();
+ }
+
+ function disableCards() {
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+ }
+
+ function unflipCards() {
+   setTimeout(() => {
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+   }, 1000);
+ }
+
   cards.forEach(card => card.addEventListener('click', flipCard));
 
-  //function setRandomCards(game) {
-   // addImgFrontCards(game)
-   // getRandomCardPosition(game)
-    //setImagesInDom(game)
-  //}
