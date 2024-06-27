45b40b48b95fef145204ddcfae37242060e89bc7
Muscardina <vergonzalezsainz@gmail.com>
2024-06-27 12:43:01 +0200

    Se añade la función de barajar y un contador de partidas provisional que aún no funciona. Pero está.

--- static/html/main.js
+++ static/html/main.js
@@ -1,43 +1,54 @@
 const cards = document.querySelectorAll('.memory-card');
 
-  let hasFlippedCard = false;
-  let firstCard, secondCard;
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
-  function flipCard() {
+function flipCard() {
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
-     return;
-   }
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
 
-   secondCard = this;
-   hasFlippedCard = false;
+    secondCard = this;
+    lockBoard = true;
 
-   checkForMatch();
- }
+    checkForMatch();
+}
 
- function checkForMatch() {
-   if (firstCard.dataset.framework === secondCard.dataset.framework) {
-     disableCards();
-     return;
-   }
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
 
-   unflipCards();
- }
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
- function disableCards() {
-   firstCard.removeEventListener('click', flipCard);
-   secondCard.removeEventListener('click', flipCard);
- }
+    resetBoard();
+}
 
- function unflipCards() {
-   setTimeout(() => {
-     firstCard.classList.remove('flip');
-     secondCard.classList.remove('flip');
-   }, 1000);
- }
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
 
-  cards.forEach(card => card.addEventListener('click', flipCard));
+        resetBoard();
+    }, 1500);
+}
 
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard))
