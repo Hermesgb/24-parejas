7468243faebe71b6ae8a3e8f727288265354532a
Muscardina <vergonzalezsainz@gmail.com>
2024-06-28 09:07:17 +0200

    Incluido un contador de puntos funcional

--- static/html/main.js
+++ static/html/main.js
@@ -1,54 +1,84 @@
-const cards = document.querySelectorAll('.memory-card');
+(() => {
+  const cards = document.querySelectorAll(".memory-card");
+  const marcador = {
+    acierto: 0,
+    aciertos: document.getElementById("aciertos"),
+    cagadas: document.getElementById("cagadas"),
+  };
 
-let hasFlippedCard = false;
-let firstCard, secondCard;
+  const cardQty = cards.length;
 
-function flipCard() {
-    this.classList.add('flip');
+  let hasFlippedCard, firstCard, secondCard;
+
+  function flipCard() {
+    this.classList.add("flip");
 
     if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-        return;
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
     }
 
     secondCard = this;
     lockBoard = true;
 
     checkForMatch();
-}
+  }
 
-function checkForMatch() {
+  function checkForMatch() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards();
-}
+  }
 
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+  function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
 
+    addPoint();
     resetBoard();
-}
+  }
 
-function unflipCards() {
+  function unflipCards() {
     setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
 
-        resetBoard();
+      resetBoard();
     }, 400);
-}
+    addCagada();
+  }
 
-function resetBoard() {
+  function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-}
+  }
 
-(function shuffle() {
-    cards.forEach(card => {
-        let ramdomPos = Math.floor(Math.random() * 12);
+  function resetGame() {
+    function shuffle() {
+      cards.forEach((card) => {
+        let ramdomPos = Math.floor(Math.random() * cardQty);
         card.style.order = ramdomPos;
-    });
-})();
+      });
+    }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+    hasFlippedCard = false;
+    marcador.acierto = 0;
+    marcador.cagada = 0;
+    shuffle();
+  }
+
+  function addPoint() {
+    marcador.acierto++;
+    marcador.aciertos.innerText = marcador.acierto;
+  }
+
+  function addCagada() {
+    marcador.cagada++;
+    marcador.cagadas.innerText = marcador.cagada;
+  }
+
+  (function startGame() {
+    cards.forEach((card) => card.addEventListener("click", flipCard));
+    resetGame();
+  })();
+})();
